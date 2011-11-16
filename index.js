06d00cf5461cad0e04b5d7cb4422448a2e8ec20b
Francesc <francesc.bgr+ifrins@gmail.com>
2011-11-16 17:18:39 +0100

    Deleted debug methods. Preparing release

--- index.js
+++ index.js
@@ -4,48 +4,53 @@ var http = require('http');
 exports.version = "0.0.1";
 
 var sendNotification = function(msg, pushUrl, type) {
-	var uri = url.parse(pushUrl);
-	var notificationClass;
+    var uri = url.parse(pushUrl);
+    var notificationClass;
 
-	var headers = {
-		"Content-Type" : "text/xml",
-		"Content-Length" : msg.length,
-		"X-NotificationClass" : 2,
-		"X-WindowsPhone-Target" : type
-	};
-	var options = {
-	  host: uri.host,
-	  port: 80,
-	  path: uri.pathname,
-	  method: 'POST',
-	  headers: headers
-	};
-	console.log("Content lenght: "+ msg.length);
-	var req = http.request(options, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-	    console.log('BODY: ' + chunk);
-	  });
-	});
+    var headers = {
+        "Content-Type": "text/xml",
+        "Content-Length": msg.length,
+        "X-NotificationClass": 2,
+        "X-WindowsPhone-Target": type
+    };
+    var options = {
+        host: uri.host,
+        port: 80,
+        path: uri.pathname,
+        method: 'POST',
+        headers: headers
+    };
 
-	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
+    var req = http.request(options, function(res) {
+		if (res.statusCode != 200) {
+			return new Error("The Push Notification wasn't recived by the MSFT Servers. " 
+								+ res.StatusCode);
+		}
+		
+        res.setEncoding('utf8');
 
-	req.write(msg);
-	req.end();
-	
+        res.on('data', function(chunk) {
+            
+        });
+    });
+
+    req.on('error', function(e) {
+		return new Error("The Push Notification wasn't recived by the MSFT Servers. " + 
+							e.message);
+    });
+
+    req.write(msg);
+    req.end();
+
 }
 
-exports.sendToastNotification = function (title, subtitle, url) {
-	var msg = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-					"<wp:Notification xmlns:wp=\"WPNotification\">" +
-					"<wp:Toast>" +
-					"<wp:Text1>" + title +"</wp:Text1>" +
-					"<wp:Text2>"+ subtitle +"</wp:Text2>" +
-					"</wp:Toast>" +
-					"</wp:Notification>";
-	return sendNotification(msg, url, 'toast');
+exports.sendToastNotification = function(title, subtitle, url) {
+    var msg = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+    "<wp:Notification xmlns:wp=\"WPNotification\">" +
+    "<wp:Toast>" +
+    "<wp:Text1>" + title + "</wp:Text1>" +
+    "<wp:Text2>" + subtitle + "</wp:Text2>" +
+    "</wp:Toast>" +
+    "</wp:Notification>";
+    return sendNotification(msg, url, 'toast');
 };
